43c99cafbe60b3f7093f6429e74d2212e8d0d955
canren2000 <128274376+canren2000@users.noreply.github.com>
2023-04-01 10:13:20 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const chatGPTBot = new ChatGPTBot();
 
 const bot =  WechatyBuilder.build({
   name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
-  puppet: "wechaty-puppet-padlocal",
+  puppet: "wechaty-puppet-wechaty",
   puppetOptions: {
     uos: true
   }
